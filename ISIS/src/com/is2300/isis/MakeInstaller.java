4be2d71d36a94edc5ccdc5501fe920084701a728
sean <sean@carricktrucking.com>
2019-09-08 11:12:24 -0500

    Sidebar Image not Loading; All Pages Done...Time to Create Installer
    I'm trying to place a custom image on the Wizard sidebar panel, but it's not working...need to look into why not.
    
    The information gathering pages are finished, with one exception...We need to change the `JList` to a `JTextArea` on the "Additional JAR Files page, and divide the paths entered with a semicolon, as I believe the Wizard API will only store the selected value of a `JList` in the settings map.
    
    Once we make that one change, we are ready to start copying files into a temporary folder, compiling the installer and generating the distributable JAR file. **Time to get coding!**

---
+++ ISIS/src/com/is2300/isis/MakeInstaller.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2019 Carrick Trucking
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.is2300.isis;
+
+import com.is2300.isis.pages.GetAddtlJARsPage;
+import com.is2300.isis.pages.GetDefInstLocPage;
+import com.is2300.isis.pages.GetJARPage;
+import com.is2300.isis.pages.GetLAPage;
+import com.is2300.isis.pages.GetLibsPage;
+import com.is2300.isis.producers.MakeInstallerProducer;
+import com.is2300.isis.pages.GetTitlePage;
+import java.awt.EventQueue;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import org.netbeans.api.wizard.WizardDisplayer;
+import org.netbeans.spi.wizard.Wizard;
+import org.netbeans.spi.wizard.WizardPage;
+
+/**
+ *
+ * @author Sean Carrick <sean at carricktrucking.com>
+ */
+public class MakeInstaller {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        final Class[] pages = new Class[] {
+            GetTitlePage.class, 
+            GetLAPage.class, 
+            GetJARPage.class, 
+            GetAddtlJARsPage.class,
+            GetLibsPage.class,
+            GetDefInstLocPage.class
+        };
+        
+//        ImageIcon image = new ImageIcon(ClassLoader.getSystemResource("com/is2300/"
+//                                          + "isis/contents/WizardSidebar.png"));
+//        if (image != null) {
+//            final ImageIcon icon = new ImageIcon(image.getImage());
+//            Dimension logoSize = new Dimension(icon.getIconWidth(), icon.getIconHeight());
+//            final BufferedImage img
+//                    = createCompatibleTranslucentImage(logoSize.width,
+//                            logoSize.height);
+//            Graphics2D g = img.createGraphics();
+//            g.setColor(Color.white);
+//            g.fillRect(0, 0, icon.getIconWidth(), icon.getIconHeight());
+//            g.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.5F));
+//            icon.paintIcon(null, g, 0, 0);
+//            g.dispose();
+//            UIManager.put("wizard.sidebar.image", createCompatibleTranslucentImage(img.getWidth(), img.getHeight())); //$NON-NLS-1$
+//        }
+        System.setProperty("wizard.sidebar.image", String.valueOf(ClassLoader.getSystemResource("com/is2300/isis/contents/WizardSidebar.png")));
+        String tmp = System.getProperty("wizard.sidebar.image");
+        tmp = tmp.replace("file:/", "file:///");
+        System.setProperty("wizard.sidebar.image", tmp);
+        
+        Runnable r;
+        r = new Runnable() {
+            public void run() {
+                MakeInstallerProducer mip = new MakeInstallerProducer();
+                int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+                int width = Toolkit.getDefaultToolkit().getScreenSize().width;
+                int x = (width - 800) / 2;
+                int y = (height - 500) / 2;
+                
+                Wizard wizard = WizardPage.createWizard("Make Installer", pages,
+                        mip);
+                WizardDisplayer.showWizard(wizard, new Rectangle(x, y, 800, 500));
+                
+                System.exit(0);
+            }
+        };
+        
+        EventQueue.invokeLater(r);
+    }
+    
+    public static BufferedImage createCompatibleTranslucentImage(int w, int h) {
+        final BufferedImage compatTransImage;
+        final GraphicsConfiguration gc = getGraphicsConfiguration();
+
+        BufferedImage timg;
+        timg = gc.createCompatibleImage(1, 1, BufferedImage.TRANSLUCENT);
+
+        if (timg.getType() == BufferedImage.TYPE_INT_ARGB_PRE) {
+            timg = new BufferedImage(1, 1, BufferedImage.TYPE_INT_ARGB);
+        }
+
+        compatTransImage = timg;
+        
+//        compatTranslImageType = compatTransImage.getType();
+
+        final ColorModel cm = compatTransImage.getColorModel();
+        final WritableRaster wr = cm.createCompatibleWritableRaster(w, h);
+        return new BufferedImage(cm, wr, cm.isAlphaPremultiplied(), null);
+    }
+
+    private static GraphicsConfiguration getGraphicsConfiguration() {
+        return GraphicsEnvironment.getLocalGraphicsEnvironment().
+                getDefaultScreenDevice().getDefaultConfiguration();
+    }
+}
